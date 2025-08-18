4cfecfd6be458d1e2e21471b9db55230992e0da5
arpit.jain <Arpit.Jain@zucitech.com>
2025-08-18 16:45:02 +0530

    Updated the swagger configuration file to send the JWT token from swagger UI

--- src/main/java/com/auth/template/config/SwaggerConfig.java
+++ src/main/java/com/auth/template/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.auth.template.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +16,13 @@ public class SwaggerConfig {
             .info(new Info()
                 .title("Auth Template API")
                 .version("1.0")
-                .description("API documentation for Auth Template application"));
+                .description("API documentation for Auth Template application"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme().name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
 
