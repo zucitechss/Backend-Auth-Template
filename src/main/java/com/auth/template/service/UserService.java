3917426c4f883d52d3d63a949fe3788da55a39bf
arpit.jain <Arpit.Jain@zucitech.com>
2025-08-26 10:19:47 +0530

    Added new feature for user tracking details

--- src/main/java/com/auth/template/service/UserService.java
+++ src/main/java/com/auth/template/service/UserService.java
@@ -1,7 +1,6 @@
 package com.auth.template.service;
 
 import com.auth.template.entity.User;
-import com.auth.template.payload.UserDTO;
 import com.auth.template.requestDTO.UserUpdateRequest;
 
 import java.util.List;
